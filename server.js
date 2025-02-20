a0d6d24206c8229b187cdfb876a51d9baf17e3e8
Akhilendra <janumalaakhilendra01122000@gmail.com>
2025-02-20 17:22:55 +0530

    first commit

--- server.js
+++ server.js
@@ -52,4 +52,4 @@ app.get("/admin/views", (req, res) => {
   res.json(viewsData);
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server Deployed`));
